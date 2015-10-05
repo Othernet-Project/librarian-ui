1711831d31242c3ae02e15c03b826314c80c62f8
Branko Vukelic <branko@brankovukelic.com>
2015-10-05 18:39:39 +0200

    Added language hooks to librarian-ui

---
+++ librarian_ui/hooks.py
@@ -0,0 +1,9 @@
+from librarian_core.contrib.templates.decorators import template_helper
+
+from . import lang
+
+
+def initialize(supervisor):
+    # This hook does not do anything. It is used primarily to register helpers
+    # from the lang module.
+    pass
